2912b6f88c8be19e69c489a2f01914bef6270d42
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-10-25 16:46:54 -0300

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1 // contador
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6) 
+
+/*
+var c = 1 // contador
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ 
+}
+*/
+//---------------------------------
+
+console.log('Vai começar...')
+for(var c = 1;c <= 6;c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
