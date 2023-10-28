1e325eb63bc82688e89a1bd774bcd68397b96d08
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-10-28 17:07:46 -0300

    consegui

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,23 +5,30 @@ function contar() {
     let res = document.querySelector('div#res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
         alert('[ERRO] Dados incompletos!')
     } else {
+        res.innerHTML = `Contando: <br>`
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        res.innerHTML = `Contagem: `
 
-        // Se inicio for menor que o fim
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+       
         if (i < f) {
+             // Contagem crescente
             for(let c = i; c <= f; c += p) {
-                res.innerHTML += `${c} `
-        } else if(i > f) {
-            for(let co = i; co >= f; co -= p) {
-                res.innerHTML += `${co} `
+                res.innerHTML += ` ${c} \u{1F449} `
+            } 
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
             }
         }
-        }
-        
+        res.innerHTML += ` \u{1F3C1} `
     }
 }
