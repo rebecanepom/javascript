7e5766b88cc6875a1f96e993e0ec7c8d8463a54a
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-10-28 18:13:42 -0300

    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,13 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.querySelector('input#num')
+    let tab = document.querySelector('select#tab')
+
+    let n = Number(num.value)
+    let c = 1
+    while (c <= 10) {
+        let item = document.createElement('option')
+        tab.innerHTML = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+        c++
+    }
+}
