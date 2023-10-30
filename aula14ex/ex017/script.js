e9cf66bf869d290ddfb873c6fdd5035cc552a538
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-10-30 13:39:03 -0300

    tab feita

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,13 +1,21 @@
 function tabuada() {
     let num = document.querySelector('input#num')
     let tab = document.querySelector('select#tab')
-
-    let n = Number(num.value)
-    let c = 1
-    while (c <= 10) {
-        let item = document.createElement('option')
-        tab.innerHTML = `${n} x ${c} = ${n*c}`
-        tab.appendChild(item)
-        c++
+    
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = `` // antes de mostrar a tab vai limpar a área
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //valor das options
+            tab.appendChild(item)
+            c++
+        }
     }
+
+    
 }
