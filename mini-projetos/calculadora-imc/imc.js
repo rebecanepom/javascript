d7a6e1a6b8e9a958d026e350e761f265647c21ba
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-12-11 17:29:41 -0300

    Update imc.js

--- mini-projetos/calculadora-imc/imc.js
+++ mini-projetos/calculadora-imc/imc.js
@@ -5,13 +5,23 @@ function calc() {
     let altura = alt.value
     let pe = document.querySelector('input#ipeso')
     let peso = pe.value
-    let imc = peso.toFixed(2)
+    let imc = peso/(altura**2)
 
     if (nome.length == 0 || altura.length == 0 || peso.length == 0) {
         res.innerHTML = 'Preencha todos os dados!'
     } else {
-        res.innerHTML = `${nome} seu IMC é ${imc} e você está ${altura} e seu peso é ${peso}`
+        res.innerHTML = `${nome} seu IMC é ${imc.toFixed(1)} e você está `
+    } if (imc <= 18.5) {
+        res.innerHTML += `abaixo do peso.`
+    } if (imc > 18.5 && imc <= 24.9) {
+        res.innerHTML += `com o peso ideal.`
+    } if (imc > 24.9 && imc <= 29.9) {
+        res.innerHTML += `levemente acima do peso.`
+    } if (imc > 29.9 && imc <= 34.9) {
+        res.innerHTML += `com obesidade grau I`
+    } if (imc > 34.9 && imc <= 39.9) {
+        res.innerHTML += `com obesidade grau II (severa).`
+    } if (imc >= 40) {
+        res.innerHTML += `com obesidade grau III (mórbida)`
     }
-
-    
 }
