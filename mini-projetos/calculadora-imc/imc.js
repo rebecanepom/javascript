82b752672068f87f6b4fe5b79e5d3724af9b2db1
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-12-12 13:37:46 -0300

    Update imc.js

--- mini-projetos/calculadora-imc/imc.js
+++ mini-projetos/calculadora-imc/imc.js
@@ -1,27 +1,25 @@
 function calc() {
-    let n = document.querySelector('input#inome')
-    let nome = n.value
-    let alt = document.querySelector('input#ialtura')
-    let altura = alt.value
-    let pe = document.querySelector('input#ipeso')
-    let peso = pe.value
+    let nome = document.querySelector('input#inome').value
+    let altura = document.querySelector('input#ialtura').value
+    let peso = document.querySelector('input#ipeso').value
     let imc = peso/(altura**2)
 
-    if (nome.length == 0 || altura.length == 0 || peso.length == 0) {
+    if (nome == '' || altura == 0 || peso == 0) {
         res.innerHTML = 'Preencha todos os dados!'
     } else {
         res.innerHTML = `${nome} seu IMC é ${imc.toFixed(1)} e você está `
-    } if (imc <= 18.5) {
-        res.innerHTML += `abaixo do peso.`
-    } if (imc > 18.5 && imc <= 24.9) {
-        res.innerHTML += `com o peso ideal.`
-    } if (imc > 24.9 && imc <= 29.9) {
-        res.innerHTML += `levemente acima do peso.`
-    } if (imc > 29.9 && imc <= 34.9) {
-        res.innerHTML += `com obesidade grau I`
-    } if (imc > 34.9 && imc <= 39.9) {
-        res.innerHTML += `com obesidade grau II (severa).`
-    } if (imc >= 40) {
-        res.innerHTML += `com obesidade grau III (mórbida)`
-    }
+        if (imc <= 18.5) {
+            res.innerHTML += `abaixo do peso.`
+        } if (imc > 18.5 && imc <= 24.9) {
+            res.innerHTML += `com o peso ideal.`
+        } if (imc > 24.9 && imc <= 29.9) {
+            res.innerHTML += `levemente acima do peso.`
+        } if (imc > 29.9 && imc <= 34.9) {
+            res.innerHTML += `com obesidade grau I`
+        } if (imc > 34.9 && imc <= 39.9) {
+            res.innerHTML += `com obesidade grau II (severa).`
+        } if (imc >= 40) {
+            res.innerHTML += `com obesidade grau III (mórbida)`
+        }
+    } 
 }
