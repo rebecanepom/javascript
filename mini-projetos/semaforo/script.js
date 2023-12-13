7b5536aef9b27d3038aed44f85d35c0a629e5f7b
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-12-13 15:38:14 -0300

    luzes

--- mini-projetos/semaforo/script.js
+++ mini-projetos/semaforo/script.js
@@ -1,2 +1,23 @@
+let semaforo = document.querySelector('div#semaforo')
+let img = document.querySelector('#imagem')
 let red = document.querySelector('button#red')
-let 
+let yellow = document.querySelector('button#yellow')
+let green = document.querySelector('button#green')
+let auto = document.querySelector('button#auto')
+
+red.addEventListener('click', luzRed)
+yellow.addEventListener('click', luzYellow)
+green.addEventListener('click', luzGreen)
+auto.addEventListener('click', luzes)
+
+function luzRed() {
+    img.setAttribute('src', 'imagens/semaforo-red.png')
+}
+
+function luzYellow() {
+    img.setAttribute('src', 'imagens/semaforo-yellow.png')
+}
+
+function luzGreen() {
+    img.setAttribute('src', 'imagens/semaforo-green.png')
+}
