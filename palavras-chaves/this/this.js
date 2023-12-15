72cceaa4b2620684f398f1d11aaeee488a064dc7
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-12-15 14:27:07 -0300

    Update this.js

--- palavras-chaves/this/this.js
+++ palavras-chaves/this/this.js
@@ -1,3 +1,30 @@
+/* O modo estrito ajuda a capturar erros comuns de codificação e impede o uso de recursos problemáticos que podem levar a bugs. Por exemplo, ele não permite a atribuição de valores a variáveis não declaradas, o que ajuda a evitar a criação acidental de variáveis globais.O modo estrito ajuda a capturar erros comuns de codificação e impede o uso de recursos problemáticos que podem levar a bugs. Por exemplo, ele não permite a atribuição de valores a variáveis não declaradas, o que ajuda a evitar a criação acidental de variáveis globais. 
+
+E como usar? Basta adicionar 'use strict' no topo do seu código, antes de qualquer outra coisa ou chama-los dentro de funções. Se você inserir dentro no topo do seu código se aplicará para todo seu código.E como usar? Basta adicionar 'use strict' no topo do seu código, antes de qualquer outra coisa ou chama-los dentro de funções. Se você inserir dentro no topo do seu código se aplicará para todo seu código.*/
+
+// Quando não estiver no modo estrito o this apontará para o objeto global
+function thisNaoEstrito() { console.log(this) }
+
+// Em modo estrito, o valor de this peranece seja qual for o definido ao entrar no contexto de execução, assim, no caso a seguir, this por padrão será indefinido (undefined)
+
+function thisEstrito() { 'use strict'; console.log(this) }
+
+thisNaoEstrito()
+thisEstrito()
+
+
+// Como método de um objeto
+
+const c = {
+    prop: 100,
+    f: function() {
+        return this.prop
+    }
+}
+
+console.log(o.f())
+
+
 // Chamar o 'this' diretamente no console nos traz o objeto global do contexto em que o JavaScript está executando.
 
 // O 'this' no devtools do chrome nos traz o objeto "window", no Node nos traz o objeto "global"
